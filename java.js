09b0e6192500ec6cb2f68fa2eef9488f8cf9b2fe
Gohssteyr <118222803+Gohssteyr@users.noreply.github.com>
2022-11-23 03:09:02 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-1").addEventListener("click", function(){
+	document.querySelector("body").setAttribute("class","bnt1");
+  })
+  
+  document.getElementById("btn-2").addEventListener("click", function(){
+	document.querySelector("body").setAttribute("class","bnt2");
+  })
+  
+  document.getElementById("btn-3").addEventListener("click", function(){
+	document.querySelector("body").setAttribute("class","bnt3");
+  })
